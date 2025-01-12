13edfdde8b572e567d7ee8f54009855089126aea
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2025-01-13 00:20:14 +0100

    Preparing report for 2025

---
+++ Scripts/1_Data_preparation_2024.R
@@ -0,0 +1,38 @@
+#In this script the data is processed and prepared for analysis and visualization
+#First,load libraries
+library(dplyr)
+library(reshape2)
+
+#Read data
+data = read.csv("Data/life_polinizadores_2024.csv")
+
+s = data %>%
+distinct(plants)
+
+
+#check structure of the data
+str(data)
+
+#create separate datasets for the two different locations
+canta <- data %>% filter(site_id=="cantavieja")
+ejea <- data %>% filter(site_id=="ejea caballeros")
+
+#select for now plant and pollinator species to create plant-pollinator networks from transects
+canta_species <- canta %>% select(c("plants", "pollinators"))
+ejea_species <- ejea %>% select(c("plants", "pollinators"))
+
+#Now aggregate pollinator visits per plant species
+canta_aggreagted <- canta_species %>% count(plants, pollinators, sort = TRUE)
+ejea_aggreagted <-  ejea_species %>% count(plants, pollinators, sort = TRUE)
+
+#Now convert to a matrix with the help of acast function from reshape library
+canta_matrix <- acast(canta_aggreagted, plants~pollinators, value.var="n")
+ejea_matrix <- acast(ejea_aggreagted, plants~pollinators, value.var="n")
+
+#Convert NA's to zeros
+canta_matrix[is.na(canta_matrix)] <- 0
+ejea_matrix[is.na(ejea_matrix)] <- 0
+
+#Save networks
+saveRDS(canta_matrix, "Data/canta_matrix_2024.rds")
+saveRDS(ejea_matrix, "Data/ejea_matrix_2024.rds")
