ee4f9fea9c4cad2e41eeff1dd86e334916caa09b
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2025-01-20 01:14:17 +0100

    report 2025

--- Scripts/7_Nestedness.R
+++ Scripts/7_Nestedness.R
@@ -21,6 +21,9 @@ nestedness_tibble = bind_rows(nestedness_networks, .id = "id")
 nestedness_tibble = nestedness_tibble %>%
 separate(id, into = c("site", "Year"), sep = "_")
 
+#Save data to avoid running it on report
+saveRDS(nestedness_tibble, "Data/nestedness.rds")
+
 nestedness_tibble %>%
 ggplot(aes(site, temperature, fill = Year), group = Year) +
 geom_col(position = "dodge", alpha=0.9) +
@@ -33,3 +36,14 @@ ggtitle("c) Nestedness")+ theme(plot.title = element_text(size=18, face= "bold")
                 axis.title = element_text(size=14),
                 axis.text = element_text(size=12))
 
+
+
+
+can_2021_nest = nestedness_tibble %>%
+filter(site == "cantavieja" & Year == "2021")
+can_2024_nest = nestedness_tibble %>%
+filter(site == "cantavieja" & Year == "2024")
+eje_2021_nest = nestedness_tibble %>%
+filter(site == "ejea" & Year == "2021")
+eje_2024_nest = nestedness_tibble %>%
+filter(site == "ejea" & Year == "2024")
--- Scripts/8_Robustness.R
+++ Scripts/8_Robustness.R
@@ -3,11 +3,14 @@
 #Load libraries
 library(dplyr)
 library(maxnodf) #for nestedness
+library(purrr)
+library(bipartite)
+library(tidyr)
+library(ggplot2)
 matrices_list = readRDS("Data/matrices_list.rds")
 
-net1 = matrices_list[[1]]
 
-
+#Compute robustness to plant loss
 robust = function(interaction_matrix){
 #The participant is the group being exterminated
 ex = second.extinct(interaction_matrix, participant="lower", method="random", nrep=1000,
@@ -15,11 +18,11 @@ ex = second.extinct(interaction_matrix, participant="lower", method="random", nr
 robustness(ex)
 }
 
-robustness_result = map(matrices_list, robust)
+robustness_plants = map(matrices_list, robust)
 
-robustness_tibble1 = bind_rows(robustness_result, .id = "id")
+robustness_plants1 = bind_rows(robustness_plants, .id = "id")
 
-long_data = robustness_tibble1 %>%
+long_plants = robustness_plants1 %>%
   pivot_longer(
     cols = everything(),
     names_to = c("site", "Year"),
@@ -27,15 +30,87 @@ long_data = robustness_tibble1 %>%
     names_sep = "_")
 
 
-long_data %>%
+long_plants %>%
 ggplot(aes(site, Robustness, fill = Year), group = Year) +
 geom_col(position = "dodge", alpha=0.9) +
 scale_fill_manual(values = c("black", "tomato4")) +
 theme_bw() +
 coord_cartesian(expand = FALSE) +
 xlab(NULL) +
-ylab("Species") +
-ggtitle("d) Robustness")+ theme(plot.title = element_text(size=18, face= "bold"), panel.border = element_rect(size=1.3),
+ylab("Robustness") +
+ggtitle("d) Robustness to plant loss")+ theme(plot.title = element_text(size=18, face= "bold"), panel.border = element_rect(size=1.3),
                 axis.title = element_text(size=14),
                 axis.text = element_text(size=12))
 
+
+
+#Compute robustness to pollinator loss
+robust = function(interaction_matrix){
+#The participant is the group being exterminated
+ex = second.extinct(interaction_matrix, participant="higher", method="random", nrep=1000,
+	details=FALSE)
+robustness(ex)
+}
+
+robustness_pollinators = map(matrices_list, robust)
+
+robustness_pollinators1 = bind_rows(robustness_pollinators, .id = "id")
+
+long_pollinators = robustness_pollinators1 %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = c("site", "Year"),
+    values_to = "Robustness",
+    names_sep = "_")
+
+
+long_pollinators %>%
+ggplot(aes(site, Robustness, fill = Year), group = Year) +
+geom_col(position = "dodge", alpha=0.9) +
+scale_fill_manual(values = c("black", "tomato4")) +
+theme_bw() +
+coord_cartesian(expand = FALSE) +
+xlab(NULL) +
+ylab("Robustness") +
+ggtitle("d) Robustness to pollinator loss")+ theme(plot.title = element_text(size=18, face= "bold"), panel.border = element_rect(size=1.3),
+                axis.title = element_text(size=14),
+                axis.text = element_text(size=12))
+
+
+#Compute robustness at network level
+robust = function(interaction_matrix){
+#The participant is the group being exterminated
+ex = second.extinct(interaction_matrix, participant="both", method="random", nrep=1000,
+	details=FALSE)
+robustness(ex)
+}
+
+robustness_network = map(matrices_list, robust)
+
+robustness_network1 = bind_rows(robustness_network, .id = "id")
+
+long_network = robustness_network1 %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = c("site", "Year"),
+    values_to = "Robustness",
+    names_sep = "_")
+
+
+long_network %>%
+ggplot(aes(site, Robustness, fill = Year), group = Year) +
+geom_col(position = "dodge", alpha=0.9) +
+scale_fill_manual(values = c("black", "tomato4")) +
+theme_bw() +
+coord_cartesian(expand = FALSE) +
+xlab(NULL) +
+ylab("Robustness") +
+ggtitle("d) Robustness network-level")+ theme(plot.title = element_text(size=18, face= "bold"), panel.border = element_rect(size=1.3),
+                axis.title = element_text(size=14),
+                axis.text = element_text(size=12))
+
+
+#Save data to avoid running it on report
+saveRDS(long_plants, "Data/robustness_plants.rds")
+saveRDS(long_pollinators, "Data/robustness_pollinators.rds")
+saveRDS(long_network, "Data/robustness_network.rds")
